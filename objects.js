3cc8d7ac76b5da6c64125f08a7e3abdb809cb82f
Emily <emily.j.langley@gmail.com>
2024-03-18 16:46:13 -0700

    added getMelonsAtPrice()

--- objects.js
+++ objects.js
@@ -2,10 +2,33 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {};
+  const splitPhrase = phrase.split(' ');
+
+  for (const word of splitPhrase) {
+    if (wordCounts[word]) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+
+  return wordCounts;
+
 }
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    2.5: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas'],
+  };
+  if (!melonPrices[price]) {
+    return null;
+  }
+
+  return melonPrices[price].sort();
+
 }
